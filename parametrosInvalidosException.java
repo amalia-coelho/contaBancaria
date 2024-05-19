53d04a00e4acaf6cdfe84c77cc7175a5d1def46d
Amália Coelho <86680741+amalia-coelho@users.noreply.github.com>
2024-05-19 19:33:49 -0300

    Exercício - Contador

---
+++ parametrosInvalidosException.java
@@ -0,0 +1,4 @@
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+    super("Parâmetros inválidos: o primeiro parâmetro deve ser menor que o segundo.");
+}	
